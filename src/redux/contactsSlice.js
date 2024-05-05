2c9ad306e5708dd6c4f886ba11befe3874f8e792
Angelina <midnightshout03@gmail.com>
2024-05-05 13:12:07 +0300

    contact slice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const contacts = {
+    items: [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ]
+};
+
+const contactSlice = createSlice({
+    name: "contacts",
+    initialState: contacts,
+    reducers: {
+        addContact(state, action) {
+            state.items = [...state.items, action.payload]
+        },
+        deleteContact(state, action) {
+            const index = state.findIndex(contact => contact.id === action.payload);
+            if (index !== -1) {
+                state.items.splice(index, 1);
+            }
+        }
+    }
+})
+
+export const { addContact, deleteContact } = contactSlice.actions;
+export default contactSlice.reducer;
